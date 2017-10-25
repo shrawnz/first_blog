0a82f79be4f446eec5b07c49329f18a939e99ade
shrawnz <shreyansm97@gmail.com>
2017-10-25 13:19:23 +0530

    Update views.py
    Add view class

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views import generic
 
 # Create your views here.
+class FirstView(generic.DetailView):
+  template_name = ''
+  def get(self,request):
+    print "GET REQUEST CALLED"
+    return 
+  
+  def post(self, request, *args, **kwargs):
+		print (request.POST)
+    return
